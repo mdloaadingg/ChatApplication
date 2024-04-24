2a492f962664588f39618f312dafc7627cef27a7
ARFA <arfaamine07@gmail.com>
2024-04-24 22:53:47 +0200

    gestion compte classes

--- src/Gestioncompte/Compte.java
+++ src/Gestioncompte/Compte.java
@@ -1,5 +1,30 @@
 package Gestioncompte;
 
-public class Compte {
+import java.io.Serializable;
 
+public class Compte implements Serializable {
+    private String pseudo;
+    private String mdp; // mot de passe
+
+    public Compte(String pseudo, String mdp) {
+        this.pseudo = pseudo;
+        this.mdp = mdp;
+    }
+
+    // Getters et setters
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public String getMdp() {
+        return mdp;
+    }
+
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
 }
--- src/Gestioncompte/CompteManager.java
+++ src/Gestioncompte/CompteManager.java
@@ -1,5 +1,65 @@
 package Gestioncompte;
 
-public class CompteManager {
+import java.rmi.server.UnicastRemoteObject;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*;
 
+public class CompteManager extends UnicastRemoteObject implements ICompte {
+    private Map<String, Compte> comptes = new HashMap<>();
+    private static final String FILE_PATH = "resources/comptes.ser"; // Chemin mis à jour
+
+    public CompteManager() throws RemoteException {
+        super();
+        loadComptes(); // Charge les comptes à la création de l'instance
+    }
+
+    @Override
+    public boolean creerCompte(String pseudo, String mdp) throws RemoteException {
+        if (comptes.containsKey(pseudo)) {
+            return false;
+        }
+        comptes.put(pseudo, new Compte(pseudo, mdp));
+        saveComptes(); // Sauvegarde après chaque création
+        return true;
+    }
+
+    @Override
+    public boolean supprimerCompte(String pseudo, String mdp) throws RemoteException {
+        Compte compte = comptes.get(pseudo);
+        if (compte != null && compte.getMdp().equals(mdp)) {
+            comptes.remove(pseudo);
+            saveComptes(); // Sauvegarde après chaque suppression
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean connexion(String pseudo, String mdp) throws RemoteException {
+        Compte compte = comptes.get(pseudo);
+        return compte != null && compte.getMdp().equals(mdp);
+    }
+
+    // Méthode pour sauvegarder les comptes dans un fichier
+    private void saveComptes() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(FILE_PATH))) {
+            out.writeObject(comptes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Méthode pour charger les comptes à partir d'un fichier
+    private void loadComptes() {
+        File file = new File(FILE_PATH);
+        if (file.exists()) {
+            try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
+                comptes = (Map<String, Compte>) in.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
